0bdcbb018e9e4fed79e9226f8264d5e5299a5f69
tute sarser <matiassarser735@gmail.com>
2023-07-01 20:36:28 -0300

    Proyecto final ultimo comit

--- js/register.js
+++ js/register.js
@@ -1,33 +1,46 @@
+// Seleccionar el elemento con el ID 'inputsRegister'
 const inputsRegister = document.getElementById('inputsRegister');
+
+// Crear un nuevo elemento div
 const div = document.createElement('div');
+
+// Agregar la clase 'formConfig' al div
 div.classList.add('formConfig');
+
+// Configurar el contenido HTML del div con el formulario de registro
 div.innerHTML = `
   <form class="inputsRegisterForm" id="registrationForm">
-  <div class= "blockInput">
-    <input type="text" placeholder="Nombre de Usuario" class="Usuario" required>
-    <input type="email" placeholder="Email" class="Email" required>
-    <input type="date" class="Date" required>
-    <input type="password" placeholder="Contraseña" class="Contraseña" required>
-    <input type="submit" value="Registrarse" class="Registrado">
+    <div class= "blockInput">
+      <input type="text" placeholder="Nombre de Usuario" class="Usuario" required>
+      <input type="email" placeholder="Email" class="Email" required>
+      <input type="date" class="Date" required>
+      <input type="password" placeholder="Contraseña" class="Contraseña" required>
+      <input type="submit" value="Registrarse" class="Registrado">
     </div>
   </form>
-
-
 `;
 
+// Agregar el div al elemento con el ID 'inputsRegister'
 inputsRegister.appendChild(div);
 
+// Obtener el formulario de registro por su ID
 const registrationForm = document.getElementById('registrationForm');
+
+// Obtener el elemento con la clase 'Registrado'
 const registradoAlert = document.querySelector('.Registrado');
 
+// Agregar un evento de escucha para el evento de envío del formulario
 registrationForm.addEventListener('submit', function(event) {
+  // Prevenir el comportamiento predeterminado del envío del formulario
   event.preventDefault();
 
+  // Obtener los valores de los campos del formulario
   const username = document.querySelector('.Usuario').value;
   const email = document.querySelector('.Email').value;
   const date = document.querySelector('.Date').value;
   const password = document.querySelector('.Contraseña').value;
 
+  // Crear un objeto 'userInfo' con la información del usuario
   const userInfo = {
     username: username,
     email: email,
@@ -35,16 +48,19 @@ registrationForm.addEventListener('submit', function(event) {
     password: password
   };
 
+  // Convertir el objeto 'userInfo' a formato JSON
   const userInfoJSON = JSON.stringify(userInfo);
 
+  // Almacenar el JSON en el localStorage con la clave 'userInfo'
   localStorage.setItem('userInfo', userInfoJSON);
 
+  // Restablecer el formulario a su estado inicial
   registrationForm.reset();
 
-  window.open('./tienda.html', "_self")
- 
-  
+  // Abrir una nueva ventana en el navegador y redirigir a './tienda.html'
+  window.open('./tienda.html', "_self");
 });
+
 
 
 
--- main.js
+++ main.js
@@ -1,33 +1,36 @@
-const modalCarrito = document.querySelector("#modalCarrito")
-const abrirCarrito = document.querySelector("#abrirCarrito")
-const cerrarCarrito = document.querySelector("#cerrarCarrito")
-const cardsProductos = document.querySelector("#cardsProductos")
-const vaciarCarrito = document.querySelector("#vaciarCarrito")
-const finalizarCompra = document.querySelector  (".Comprar")
-const agregarCarritos = document.querySelectorAll(".agregarCarrito")
+// Seleccionar elementos del DOM y del HTML
+const modalCarrito = document.querySelector("#modalCarrito");
+const abrirCarrito = document.querySelector("#abrirCarrito");
+const cerrarCarrito = document.querySelector("#cerrarCarrito");
+const cardsProductos = document.querySelector("#cardsProductos");
+const vaciarCarrito = document.querySelector("#vaciarCarrito");
+const finalizarCompra = document.querySelector(".Comprar");
+const agregarCarritos = document.querySelectorAll(".agregarCarrito");
 
+// Declarar variables para el carrito y el precio total
 let carrito = [];
-let precioTotal = 0
+let precioTotal = 0;
 
-//guardar carrito cuando se actualice la pagina
+// Cargar carrito desde el localStorage al cargar la página
 document.addEventListener("DOMContentLoaded", () => {
   carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-  verCarrito();})
+  verCarrito();
+});
 
-
-
-//boton para abrir el carrito
+// Abrir el carrito al hacer clic en el botón correspondiente
 abrirCarrito.addEventListener("click", () => {
-  modalCarrito.showModal()
-})
-//boton para cerrar el carrito
+  modalCarrito.showModal();
+});
+
+// Cerrar el carrito al hacer clic en el botón correspondiente
 cerrarCarrito.addEventListener("click", () => {
-  modalCarrito.close()
-})
-//boton para vaciar carrito con libreria
-vaciarCarrito.addEventListener('click', ()=>{
+  modalCarrito.close();
+});
+
+// Vaciar el carrito al hacer clic en el botón correspondiente
+vaciarCarrito.addEventListener('click', () => {
   Toastify({
-    classname:"alert",
+    classname: "alert",
     text: "Carrito Vaciado",
     duration: 1000,
     newWindow: true,
@@ -41,105 +44,24 @@ vaciarCarrito.addEventListener('click', ()=>{
     onClick: function(){} // Callback after click
   }).showToast();
 
-  carrito.length =[]
-  verCarrito()})
+  carrito.length = 0;
+  verCarrito();
+});
 
+// Función para obtener los datos del archivo JSON
+const jsonFetch = async () => {
+  const respuesta = await fetch("../data.json");
+  const sneakers = await respuesta.json();
+  return sneakers;
+};
 
-  const jsonFethc = async() =>{
-    const respuesta = await fetch ("../data.json")
-    const sneakers = await respuesta.json()
+// Crear los productos en el DOM
+const crearProductos = () => {
+  cardsProductos.innerHTML = "";
 
-    return sneakers
-  }
-//creacion de productos
-  const crearProductos = () => {
-    cardsProductos.innerHTML = "";
-  
-    jsonFethc().then(sneakers => {
-      sneakers.forEach(producto => {
-        const card = document.createElement("div");
-        card.innerHTML = `
-          <div class="producto">
-            <img src="${producto.imagen}" alt="imagen del producto" class="imgProducto">
-            <div class="nombreProducto">${producto.nombre}</div>
-            <div class="precioProducto">$${producto.precio}</div>
-            <div class="botonProducto">
-              <button id="${producto.id}" class="agregarCarrito">+</button>
-            </div>
-          </div>
-        `;
-        cardsProductos.appendChild(card);
-  
-        const btnAgregarCarrito = document.getElementById(`${producto.id}`);
-        btnAgregarCarrito.addEventListener("click", (e) => agregarCarrito(e, sneakers));
-  
-        btnAgregarCarrito.addEventListener("click", () => {
-          Toastify({
-            classname: "alert",
-            text: "Producto agregado",
-            duration: 1000,
-            newWindow: true,
-            close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "center", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-              background: "linear-gradient(to left, ) class= alert",
-            },
-            onClick: function(){} // Callback after click
-          }).showToast();
-        });
-      });
-  //filtro de precios
-      const filtroPrecios = document.createElement("div")
-      filtroPrecios.innerHTML = `
-      <div class="displayFiltros">
-      <div>
-        <label class="textPrecioMin" for="precioMin">Precio mínimo:</label>
-        <input class="precioMin" type="number" id="precioMin" min="0" step="1">
-        </div>
-        <div>
-        <label class="textPrecioMax" for="precioMax">Precio máximo:</label>
-        <input  class="precioMax" type="number" id="precioMax" min="0" step="1">
-        </div>
-        <div>
-        <button class="Filter" id="btnFiltrar">Filtrar</button>
-        <button class="LimpiarFilter" id="btnLimpiarFiltro">Limpiar filtro</button>
-        </div>
-        </div>
-      `;
-      cardsProductos.insertAdjacentElement("beforebegin", filtroPrecios)
-      const btnFiltrar = document.getElementById("btnFiltrar")
-      const btnLimpiarFiltro = document.getElementById("btnLimpiarFiltro")
-      //boton de filtrar
-      btnFiltrar.addEventListener("click", () => {
-        const precioMin = parseFloat(document.getElementById("precioMin").value)
-        const precioMax = parseFloat(document.getElementById("precioMax").value)
-        
-  
-        const productosFiltrados = filtrarPorPrecio(sneakers, precioMin, precioMax)
-        mostrarProductosFiltrados(productosFiltrados);
-      
-
-      });
-      
-      //boton de limpiar el filtro
-      btnLimpiarFiltro.addEventListener("click", () => {
-        document.getElementById("precioMin").value = ""
-        document.getElementById("precioMax").value = ""
-        mostrarProductosFiltrados(sneakers);
-      });
-    });
-  };
-  
-  function filtrarPorPrecio(productos, min, max) {
-    return productos.filter(producto => producto.precio >= min && producto.precio <= max)
-  }
-  
-  function mostrarProductosFiltrados(productos) {
-    cardsProductos.innerHTML = "";
-    productos.forEach(producto => {
-      const card = document.createElement("div")
+  jsonFetch().then(sneakers => {
+    sneakers.forEach(producto => {
+      const card = document.createElement("div");
       card.innerHTML = `
         <div class="producto">
           <img src="${producto.imagen}" alt="imagen del producto" class="imgProducto">
@@ -150,36 +72,118 @@ vaciarCarrito.addEventListener('click', ()=>{
           </div>
         </div>
       `;
-      cardsProductos.appendChild(card)
-    })
+      cardsProductos.appendChild(card);
+
+      const btnAgregarCarrito = document.getElementById(`${producto.id}`);
+      btnAgregarCarrito.addEventListener("click", (e) => agregarCarrito(e, sneakers));
+
+      btnAgregarCarrito.addEventListener("click", () => {
+        Toastify({
+          classname: "alert",
+          text: "Producto agregado",
+          duration: 1000,
+          newWindow: true,
+          close: true,
+          gravity: "top", // `top` or `bottom`
+          position: "center", // `left`, `center` or `right`
+          stopOnFocus: true, // Prevents dismissing of toast on hover
+          style: {
+            background: "linear-gradient(to left, ) class= alert",
+          },
+          onClick: function(){} // Callback after click
+        }).showToast();
+      });
+    });
+
+    // Crear filtro de precios
+    const filtroPrecios = document.createElement("div");
+    filtroPrecios.innerHTML = `
+      <div class="displayFiltros">
+        <div>
+          <label class="textPrecioMin" for="precioMin">Precio mínimo:</label>
+          <input class="precioMin" type="number" id="precioMin" min="0" step="1">
+        </div>
+        <div>
+          <label class="textPrecioMax" for="precioMax">Precio máximo:</label>
+          <input class="precioMax" type="number" id="precioMax" min="0" step="1">
+        </div>
+        <div>
+          <button class="Filter" id="btnFiltrar">Filtrar</button>
+          <button class="LimpiarFilter" id="btnLimpiarFiltro">Limpiar filtro</button>
+        </div>
+      </div>
+    `;
+    cardsProductos.insertAdjacentElement("beforebegin", filtroPrecios);
+
+    const btnFiltrar = document.getElementById("btnFiltrar");
+    const btnLimpiarFiltro = document.getElementById("btnLimpiarFiltro");
+
+    // Evento del botón "Filtrar"
+    btnFiltrar.addEventListener("click", () => {
+      const precioMin = parseFloat(document.getElementById("precioMin").value);
+      const precioMax = parseFloat(document.getElementById("precioMax").value);
+
+      const productosFiltrados = filtrarPorPrecio(sneakers, precioMin, precioMax);
+      mostrarProductosFiltrados(productosFiltrados);
+    });
+
+    // Evento del botón "Limpiar filtro"
+    btnLimpiarFiltro.addEventListener("click", () => {
+      document.getElementById("precioMin").value = "";
+      document.getElementById("precioMax").value = "";
+      mostrarProductosFiltrados(sneakers);
+    });
+  });
+};
+
+// Función para filtrar productos por precio
+function filtrarPorPrecio(productos, min, max) {
+  return productos.filter(producto => producto.precio >= min && producto.precio <= max);
+}
+
+// Función para mostrar los productos filtrados
+function mostrarProductosFiltrados(productos) {
+  cardsProductos.innerHTML = "";
+  productos.forEach(producto => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+      <div class="producto">
+        <img src="${producto.imagen}" alt="imagen del producto" class="imgProducto">
+        <div class="nombreProducto">${producto.nombre}</div>
+        <div class="precioProducto">$${producto.precio}</div>
+        <div class="botonProducto">
+          <button id="${producto.id}" class="agregarCarrito">+</button>
+        </div>
+      </div>
+    `;
+    cardsProductos.appendChild(card);
+  });
+}
+
+// Función para agregar un producto al carrito
+function agregarCarrito(e, productos) {
+  const id = e.target.id;
+  const productoEncontrado = productos.find(producto => producto.id === id);
+  const sumarProd = carrito.find(prod => prod.id === id);
+
+  if (sumarProd) {
+    sumarProd.cantidad++;
+  } else {
+    carrito.push({ ...productoEncontrado, cantidad: 1 });
   }
-    
-     
-  function agregarCarrito(e, productos){
-      const id = e.target.id
 
-      const productoEncontrado= productos.find((productos) => productos.id === id)
-      const sumarProd = carrito.find(prod => prod.id == id) 
+  verCarrito();
+}
 
-      if (sumarProd) {
-        sumarProd.cantidad++
-      } else {
-        carrito.push({...productoEncontrado, cantidad: 1})
-      }
-      verCarrito()
-      
+// Crear productos al cargar la página
+crearProductos();
 
-    }
-
-
-crearProductos()
-
-//Ver productos dentro del carrito
+// Función para ver los productos dentro del carrito
 const verCarrito = () => {
-  const carritoContainer = document.querySelector("#modalContainer")
-  carritoContainer.innerHTML = ""
+  const carritoContainer = document.querySelector("#modalContainer");
+  carritoContainer.innerHTML = "";
 
-  let carritoHTML = ""
+  let carritoHTML = "";
 
   carrito.forEach(prod => {
     carritoHTML += `
@@ -198,80 +202,78 @@ const verCarrito = () => {
           <button class="eliminarProd">Eliminar</button>
         </div>
       </div>
-    `
-  })
+    `;
+  });
 
-  const cantidades = carrito.reduce((a, b) => a + b.cantidad, 0)
-  precioTotal = carrito.reduce((a, b) => a + b.precio * b.cantidad, 0)
-  guardarStorage()
-  
-  
+  const cantidades = carrito.reduce((a, b) => a + b.cantidad, 0);
+  precioTotal = carrito.reduce((a, b) => a + b.precio * b.cantidad, 0);
+  guardarStorage();
+
   carritoHTML += `
-    <div class= "precioCant">
-    <div class = "cantidadTot">
-      <p class="canTCarrito">Cantidad Total:</p>
-      <p class="canTCarrito">${cantidades}</p>
-    </div>  
-    <div class ="PrecioTot">
-      <p class="preciototal">Precio Total: $</p>
-      <p class="preciototal">${precioTotal}</p>
+    <div class="precioCant">
+      <div class="cantidadTot">
+        <p class="canTCarrito">Cantidad Total:</p>
+        <p class="canTCarrito">${cantidades}</p>
+      </div>  
+      <div class="PrecioTot">
+        <p class="preciototal">Precio Total: $</p>
+        <p class="preciototal">${precioTotal}</p>
+      </div>
     </div>
-    </div>
-  `
+  `;
 
-  carritoContainer.innerHTML = carritoHTML
+  carritoContainer.innerHTML = carritoHTML;
 
- 
-  const eliminarProd = document.querySelectorAll(".eliminarProd")
-//eliminar, agregar ud y eliminar ud
+  const eliminarProd = document.querySelectorAll(".eliminarProd");
+
   eliminarProd.forEach(botones => {
     botones.addEventListener("click", () => {
-      const prodId = botones.parentNode.parentNode.attributes.prodId.value
-      const prodIndex = carrito.findIndex(prod => prod.id == prodId)
+      const prodId = botones.parentNode.parentNode.attributes.prodId.value;
+      const prodIndex = carrito.findIndex(prod => prod.id === prodId);
 
-      carrito.splice(prodIndex, 1)
-      verCarrito()
-    })
-  })
+      carrito.splice(prodIndex, 1);
+      verCarrito();
+    });
+  });
 
-  const agregarUd = document.querySelectorAll(".agregarUd")
+  const agregarUd = document.querySelectorAll(".agregarUd");
 
   agregarUd.forEach(botones => {
     botones.addEventListener("click", () => {
-      const prodId = botones.parentNode.parentNode.attributes.prodId.value
-      const prod = carrito.find(prod => prod.id == prodId)
-      prod.cantidad++
+      const prodId = botones.parentNode.parentNode.attributes.prodId.value;
+      const prod = carrito.find(prod => prod.id === prodId);
+      prod.cantidad++;
 
-      verCarrito()
-    })
-  })
+      verCarrito();
+    });
+  });
 
-  const eliminarUd = document.querySelectorAll(".eliminarUd")
+  const eliminarUd = document.querySelectorAll(".eliminarUd");
 
   eliminarUd.forEach(botones => {
     botones.addEventListener("click", () => {
-      const prodId = botones.parentNode.parentNode.attributes.prodId.value
-      const prod = carrito.find(prod => prod.id == prodId)
-      const prodIndex = carrito.findIndex(prod => prod.id == prodId)
+      const prodId = botones.parentNode.parentNode.attributes.prodId.value;
+      const prod = carrito.find(prod => prod.id === prodId);
+      const prodIndex = carrito.findIndex(prod => prod.id === prodId);
 
-      prod.cantidad--
+      prod.cantidad--;
 
       if (prod.cantidad < 1) {
-        carrito.splice(prodIndex, 1)
+        carrito.splice(prodIndex, 1);
       }
 
-      verCarrito()
+      verCarrito();
+    });
+  });
+};
 
-    })
-  })
-}
-//boton para finalizar la compra
+// Botón para finalizar la compra
 finalizarCompra.addEventListener("click", () => {
   const userInfo = localStorage.getItem("userInfo");
   const compraId = IdUnico();
 
-  if (userInfo == null) {
-    window.open('./register.html', "_self");
+  if (userInfo === null) {
+    window.open("./register.html", "_self");
   } else {
     Toastify({
       classname: "alert",
@@ -280,28 +282,27 @@ finalizarCompra.addEventListener("click", () => {
       newWindow: true,
       close: true,
       gravity: "top", // `top` or `bottom`
-      position: "centar", // `left`, `center` or `right`
+      position: "center", // `left`, `center` or `right`
       stopOnFocus: true, // Prevents dismissing of toast on hover
       style: {
         background: "linear-gradient(to left, ) class= alert",
       },
-      onClick: function(){} // Callback after click
+      onClick: function() {} // Callback after click
     }).showToast();
     const compra = { articulos: [...carrito], total: precioTotal };
 
-   
     localStorage.setItem(`Compra-${compraId}`, JSON.stringify(compra));
-
     carrito = [];
     verCarrito();
   }
 });
 
-//id unico para la compra 
+// Generar un ID único para cada compra
 function IdUnico() {
-  return Math.random().toString(36);
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
 }
+
+// Guardar el carrito en el localStorage
 function guardarStorage() {
   localStorage.setItem("carrito", JSON.stringify(carrito));
 }
-
