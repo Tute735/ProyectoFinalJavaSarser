591c1540943dadfbaf51e4002b499f071579a0da
tute sarser <matiassarser735@gmail.com>
2023-07-01 16:41:41 -0300

    proyecto final 1.8

--- js/register.js
+++ js/register.js
@@ -1,45 +1,47 @@
-const inputsRegister = document.getElementById('inputsRegister')
-const div = document.createElement('div')
-div.classList.add()
+const inputsRegister = document.getElementById('inputsRegister');
+const div = document.createElement('div');
+div.classList.add('formConfig');
 div.innerHTML = `
-<form id="registrationForm" class="formConfig">
-  <input type="text" placeholder="Nombre de Usuario" class="Usuario" required>
-  <input type="email" placeholder="Email" class="Email" required>
-  <input type="date" class="Date" required>
-  <input type="password" placeholder="Contraseña" class="Contraseña" required>
-  <input type="submit" value="Registrarse" class="Registrado">
-</form>
-<div id="message"></div>
-`
-inputsRegister.appendChild(div)
+  <form id="registrationForm">
+    <input type="text" placeholder="Nombre de Usuario" class="Usuario" required>
+    <input type="email" placeholder="Email" class="Email" required>
+    <input type="date" class="Date" required>
+    <input type="password" placeholder="Contraseña" class="Contraseña" required>
+    <input type="submit" value="Registrarse" class="Registrado">
+  </form>
+  <div id="message"></div>
+`;
 
-const registrationForm = document.getElementById('registrationForm')
-const messageDiv = document.getElementById('message')
+inputsRegister.appendChild(div);
+
+const registrationForm = document.getElementById('registrationForm');
+const messageDiv = document.getElementById('message');
+const registradoAlert = document.querySelector('.Registrado');
 
 registrationForm.addEventListener('submit', function(event) {
-  event.preventDefault()
+  event.preventDefault();
 
-  const username = document.querySelector('.Usuario').value
-  const email = document.querySelector('.Email').value
-  const date = document.querySelector('.Date').value
-  const password = document.querySelector('.Contraseña').value
+  const username = document.querySelector('.Usuario').value;
+  const email = document.querySelector('.Email').value;
+  const date = document.querySelector('.Date').value;
+  const password = document.querySelector('.Contraseña').value;
 
   const userInfo = {
     username: username,
     email: email,
     date: date,
     password: password
-  }
- 
-  const userInfoJSON = JSON.stringify(userInfo)
+  };
 
-  localStorage.setItem('userInfo', userInfoJSON)
+  const userInfoJSON = JSON.stringify(userInfo);
 
-  registrationForm.reset()
+  localStorage.setItem('userInfo', userInfoJSON);
 
+  registrationForm.reset();
 
-})
-
+  messageDiv.textContent = 'Registrado con éxito, ya puedes comprar en la tienda';
+  messageDiv.classList.add('RegistradoExito');
+});
 
 
 
